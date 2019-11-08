2b01a45dec52006674d4ee743c342623b3660b3f
Vince Lollino <charlie_fog@comcast.net>
2019-11-07 19:31:12 -0600

    created routing dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-// import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component'; // <-- NgModel lives here
+import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component'; // <-- NgModel lives here
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule,
-    FormsModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [
     // no need to place any providers due to the `providedIn` flag...
