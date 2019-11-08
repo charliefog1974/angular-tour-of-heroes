78878a9739d5cc6409ce198dc89841604c48f2b4
Vince Lollino <charlie_fog@comcast.net>
2019-11-07 18:41:36 -0600

    created messaging service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,21 +10,39 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-   heroes = HEROES;
-   selectedHero: Hero;
+  selectedHero: Hero;
+
+  heroes: Hero[];
+
+  //  heroes = HEROES;
+  
 
   // selectedHero: Hero = {
   //   id: 1,
   //   name: 'Windstorm'
   // }
 
-  constructor() { }
+  // constructor() { } added a private heroService param to the constructor
+  constructor(private heroService: HeroService) { }
+
+  // ngOnInit() {
+  // }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
-    this.selectedHero = hero;  
+    this.selectedHero = hero;
+  }
+
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // } replaced after observable, of was added to hero.service.ts
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
   }
 
 }
